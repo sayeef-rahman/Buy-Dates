cb3f168cac4ed454a27c1fa8e8fdfdb9ef7e0650
sayeef-rahman <sayeef.cse@gmail.com>
2022-03-26 01:37:55 +0600

    4 product add to cart condition added/ finished

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const[cart, setCart] = useState([]);
 
     useEffect(() =>{
         fetch('products.json')
@@ -11,14 +13,23 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[]);
 
-    const handleAddToCart = () =>{
-        console.log("clicled");
+    const handleAddToCart = (products) =>{
+        // cart = products;
+        // console.log(products);
+        console.log("Cart: ",cart);
+        const newCart = [...cart,products];
+        console.log("New Cart Length: ",newCart.length);
+        if(newCart.length <= 4){
+            setCart(newCart);
+        }
+        
     }
 
     return (
         <div className='shop-container'>
             <div className="cart-container">
                 <p>Cart</p>
+                <p>Selected: {cart.map(date => date.name)}</p>
             </div>
             <div className="products-container">
             {
