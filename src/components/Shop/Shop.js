e481f4d40297979abc7af3e3155fc653af10178f
sayeef-rahman <sayeef.cse@gmail.com>
2022-03-25 22:10:28 +0600

    general file added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="cart-container">
+                <p>Cart</p>
+            </div>
+            <div className="products-container">
+                <p>In Shop</p>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
