b5947ad698564829d84c0a7fff10fb966f2025e9
sayeef-rahman <sayeef.cse@gmail.com>
2022-03-25 23:48:50 +0600

    fetch completed- data received

---
+++ src/components/Product/Product.js
@@ -0,0 +1,15 @@
+// import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <p>hi</p>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[]);
     return (
         <div className='shop-container'>
             <div className="cart-container">
                 <p>Cart</p>
             </div>
             <div className="products-container">
-                <p>In Shop</p>
+            {
+                    products.map(product =><Product
+                         key={product.id}
+                         product={product}
+                         >
+                    </Product>)
+                }
             </div>
             
         </div>
